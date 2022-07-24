cc1ab39868ac724243c9236c9936a19c9c4f0c0a
captainAyan <ayanchakraborty105@gmail.com>
2022-07-24 21:28:02 +0530

    fix: :bug: fixed bugs that appeared after upgradation of the dependencies

--- client/client.js
+++ client/client.js
@@ -1,19 +1,25 @@
-var io = require('socket.io-client');
-const socket = io('https://socketkeyboard.herokuapp.com/', {query: 'type=listener'});
-const sendkeys = require('sendkeys');
+var io = require("socket.io-client");
+const socket = io("https://socketkeyboard.herokuapp.com/", {
+  query: "type=listener",
+});
+const sendkeys = require("sendkeys");
 
 var uuid = "";
 
 socket.on("connection-ack", (data) => {
-    socket.off("resp-"+uuid);
-    uuid = data.uuid;
+  socket.off("resp-" + uuid);
+  uuid = data.uuid;
 
-    console.log("New UUID --> " + uuid);
+  console.log("New UUID --> " + uuid);
 
-    socket.on("resp-"+uuid, (data)=> {
-        for(var i=0; i<data.message.length; i++) {
-            try {sendkeys.sync(data.message[i]);}
-            catch(e) {console.log("Some characters will not be typed");}
-        }
-    })
-})
+  socket.on("resp-" + uuid, (data) => {
+    console.log(data.message);
+    for (var i = 0; i < data.message.length; i++) {
+      try {
+        sendkeys.sync(data.message[i]);
+      } catch (e) {
+        console.log("Some characters will not be typed");
+      }
+    }
+  });
+});
